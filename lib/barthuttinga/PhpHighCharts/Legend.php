9dbb4d64849cae424a5753610afdf2ee61941cc8
Bart Huttinga <bart@qaraqter.nl>
2014-04-01 11:45:40 +0200

    add some properties to legend

--- lib/barthuttinga/PhpHighCharts/Legend.php
+++ lib/barthuttinga/PhpHighCharts/Legend.php
@@ -7,4 +7,24 @@ class Legend extends Base
      * @var boolean
      */
     protected $enabled;
+
+    /**
+     * @var string
+     */
+    protected $backgroundColor;
+
+    /**
+     * @var string
+     */
+    protected $borderColor;
+
+    /**
+     * @var float
+     */
+    protected $borderWidth;
+
+    /**
+     * @var float
+     */
+    protected $borderRadius;
 }
