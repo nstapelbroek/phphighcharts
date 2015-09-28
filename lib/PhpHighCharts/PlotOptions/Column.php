0f90b559a2d8c5d664d382ada1f0da338eb26b7c
martenwilmink <marten@qaraqter.nl>
2015-09-28 17:31:17 +0200

    add pointWidth to column, add phpstorm to gitignore

--- lib/PhpHighCharts/PlotOptions/Column.php
+++ lib/PhpHighCharts/PlotOptions/Column.php
@@ -19,4 +19,9 @@ class Column extends Base
      * @var PhpHighCharts\PlotOptions\Series\DataLabels
      */
     protected $dataLabels;
+
+    /**
+     * @var integer
+     */
+    protected $pointWidth;
 }
